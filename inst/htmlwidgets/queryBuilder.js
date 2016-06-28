e0da349f79d312a5cad90374a680911d13a144be
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-06-28 19:02:36 -0400

    Simple test to make sure widget is working

--- inst/htmlwidgets/queryBuilder.js
+++ inst/htmlwidgets/queryBuilder.js
@@ -12,8 +12,34 @@ HTMLWidgets.widget({
 
       renderValue: function(x) {
 
-        // TODO: code to render the widget, e.g.
-        el.innerText = x.message;
+var rules_basic = {
+  condition: 'AND',
+  rules: [{
+    id: 'price',
+    operator: 'less',
+    value: 10.25
+  }, {
+    condition: 'OR',
+    rules: [{
+      id: 'category',
+      operator: 'equal',
+      value: 2
+    }, {
+      id: 'category',
+      operator: 'equal',
+      value: 1
+    }]
+  }]
+};
+
+        $(el).queryBuilder({
+          filters:[{
+            id: 'name',
+            label: 'Name',
+            type: 'string'
+          }],
+          rules: rules_basic
+        });
 
       },
 
