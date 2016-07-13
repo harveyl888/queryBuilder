447626e26c34ce35b95502220a0c029814889e70
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-07-13 12:14:10 -0400

    clean up code

--- inst/htmlwidgets/queryBuilder.js
+++ inst/htmlwidgets/queryBuilder.js
@@ -85,129 +85,7 @@ HTMLWidgets.widget({
         operator.push({ type: "is_not_na", "nb_inputs": "0", "apply_to": ["number", "string", "datetime", "boolean"] });
         operator.push({ type: "is_na", "nb_inputs": "0", "apply_to": ["number", "string", "datetime", "boolean"] });
 
-
-
-
-
-/*
-        // Generate json strings from x.data
-        var jsonString;  // string to store json-formatted filter
-        var filter = [];  // array to store all the filters
-        x.data.forEach(function(i) {
-          jsonString = "{ id: '" + i.name + "', label: '" + i.name + "', type: '" + i.type + "'";
-          if (i.hasOwnProperty('input')) {
-            if (i.input != 'selectize') {
-              jsonString += ", input: '" + i.input + "'";
-            }
-          }
-          if (i.type == 'integer' || i.type == 'double') {
-            var myProps = ['min', 'max', 'step'];
-            if (i.hasOwnProperty('min') || i.hasOwnProperty('max') || i.hasOwnProperty('step')) {
-              jsonString += ", validation: {";
-              var addjsonNum = [];
-              for (var j in myProps) {
-                if (i.hasOwnProperty(myProps[j])) { addjsonNum.push(myProps[j] + ": " + i[myProps[j]]); }
-              }
-              jsonString += addjsonNum.join() + '}';
-            }
-          }
-          if (i.input == 'select' || i.input == 'radio') {
-            if (i.hasOwnProperty('values')) {
-              jsonString += ", values: [";
-              var addjsonSelect = [];
-
-              for (var k = 0; k < i.values.length; k++) {
-                addjsonSelect.push("'" + i.values[k] + "'");
-              }
-              jsonString += addjsonSelect.join(', ') + ']';
-            }
-          } else if (i.input == 'selectize') {
-            if (i.hasOwnProperty('values')) {
-              jsonString += ", plugin: 'selectize', plugin_config: { ";
-              jsonString += "valueField: 'id', labelField: 'id', maxItems: null, create: false, ";
-              jsonString += "options: [";
-              var addjsonSelectize = [];
-              for (var k1 = 0; k1 < i.values.length; k1++) {
-                addjsonSelectize.push("{id: '" + i.values[k1] + "'}");
-              }
-              jsonString += addjsonSelectize.join(', ') + '] }';
-
-
-
-//              jsonString += ', "valueGetter": "function(rule) { var value = []; rule.$el.find(\'.rule-value-container input\').each(function() { value.push($(this).selectize()[0].selectize.items) }); return(value) }"';
-
-
-
-              jsonString += ", valueGetter: function(rule) { return rule.$el.find(\'.selectized\').selectize()[0].selectize.items; }";
-              }
-
-//             $("#input-tags").selectize()[0].selectize.items
-
-          } else if (i.type == 'date') {
-            jsonString += ", plugin: 'datepicker', plugin_config: { format: 'yyyy/mm/dd', todayBtn: 'linked', todayHighlight: true, autoclose: true }";
-          }
-
-
-          if (i.input == 'selectize') {
-            jsonString += ", operators: [ 'in', 'not_in' ]";
-          } else if (i.input == 'select' || i.input == 'radio') {
-            jsonString += ", operators: [ 'equal', 'not_equal', 'is_na', 'is_not_na' ]";
-          } else if (i.hasOwnProperty('operators')) {
-            var addjsonOperators = [];
-            for (var op in i.operators) {
-              addjsonOperators.push("'" + i.operators[op] + "'");
-            }
-            jsonString += ", operators: [" + addjsonOperators.join(", ") + "]";
-          } else if (i.type == 'integer' || i.type == 'double' || i.type == 'date') {
-            jsonString += ", operators: [" + opObj.numeric + "]";
-          } else if (i.type == 'text') {
-            jsonString += ", operators: [" + opObj.text + "]";
-          }
-
-
-          jsonString += "}";
-          filter.push(jsonString);  // add this filter to the filter array
-        });
-        var jsonFilter = "[" + filter.join() + "]";
-    //    var jsonFilter = JSON.parse("[" + filter.join() + "]");  // parse all the filters
-        Shiny.onInputChange(el.id + '_filters', '[' + filter.join() + ']');
-
-        // for debugging
-        window.jsonFilter = jsonFilter;
-
-        var myOperators = ['equal', 'not_equal', 'less', 'less_or_equal', 'greater', 'greater_or_equal', 'between', 'not_between', 'begins_with', 'not_begins_with', 'ends_with', 'not_ends_with', 'contains', 'not_contains', 'in', 'not_in'];
-        var operator = [];
-        for (var j in myOperators) {
-          operator.push("{ type: '" + myOperators[j] + "' }");
-        }
-        operator.push("{ type: 'is_not_na', 'nb_inputs': '0', 'apply_to': ['number', 'string', 'datetime', 'boolean'] }");
-        operator.push("{ type: 'is_na', 'nb_inputs': '0', 'apply_to': ['number', 'string', 'datetime', 'boolean'] }");
-
-//        var jsonOperators =  JSON.parse("[" + operator.join() + "]");  // parse all the operators
-        var jsonOperators =  "[" + operator.join() + "]";  // parse all the operators
-
-        // initialize validate status to false
-        Shiny.onInputChange(el.id + '_validate', false);
-
-        // add a fix for selectize
-        $(el).on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-                                                        if (rule.filter.plugin == 'selectize') {
-                                                        rule.$el.find('.rule-value-container').css('min-width', '200px')
-                                                        .find('.selectize-control').removeClass('form-control');
-                                                      }
-                                                    });
-      alert(jsonFilter);
-
-
-        // build the query
-        $(el).queryBuilder({
-          filters: jsonFilter,
-          operators: jsonOperators
-        });
-
-
-
-*/
+        // return filter as stringified JSON
         Shiny.onInputChange(el.id + '_filters', JSON.stringify(filter));
 
         // initialize validate status to false
@@ -221,14 +99,15 @@ HTMLWidgets.widget({
                                                       }
                                                     });
 
+        // for debugging
+        window.filterout = filter;
+        window.operatorout = operator;
 
-window.filterout = filter;
-window.operatorout = operator;
-
-$(el).queryBuilder({
-  filters: filter,
-  operators: operator
-});
+        // build the query
+        $(el).queryBuilder({
+          filters: filter,
+          operators: operator
+        });
 
 
         // don't display errors
